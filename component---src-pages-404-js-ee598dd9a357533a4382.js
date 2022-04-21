40a82d31c70abb937eb0303088be239db284a8f5
flrngel <flrngel@gmail.com>
2022-04-21 12:55:37 +0900

    Revert "update"
    This reverts commit 6a16f946b82c92d0bf7ff22e42cd00ed9593eafc.

---
+++ component---src-pages-404-js-ee598dd9a357533a4382.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a);e.default=function(){return o.a.createElement(o.a.Fragment,null,"404, page not found!")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ee598dd9a357533a4382.js.map
